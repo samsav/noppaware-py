f32388847fc6100de666e02b38b55db006597db5
Sampo Savolainen <savolainen.sampo@gmail.com>
2019-03-10 15:25:29 +0200

    Add comments to code

--- noppaware.py
+++ noppaware.py
@@ -2,8 +2,14 @@
 
 import secrets
 
+# SystemRandom() uses the best available OS-specific source for generating
+# random numbers. On Unix-based systems, dev/urandom is used; on Windows,
+# CryptGenRandom() is used. See https://docs.python.org/3/library/secrets.html
+# and https://docs.python.org/3/library/os.html#os.urandom
 RND = secrets.SystemRandom()
 
+# Open the Noppaware list and read the contents into a dictionary with the
+# number IDs as keys and words as values
 with open("noppaware.txt", 'r') as fp:
     WORDS = {line.split()[0]: line.split()[1] for line in fp.readlines()}
 
@@ -11,12 +17,16 @@ N_PHRASES = 4
 PHRASE_LENGTH = 7
 PHRASES = list()
 
+# Loops for generating phrases
 for i in range(N_PHRASES):
     phrase = list()
     for j in range(PHRASE_LENGTH):
+        # Emulate the dice rolling process by randomly generating 5 numbers in
+        # range 1–6 and concatenating them into a number ID
         key = ''.join([str(RND.randint(1, 6)) for k in range(5)])
         phrase.append(WORDS[key])
     PHRASES.append(phrase)
 
+# Loop for printing out the phrases
 for phrase in PHRASES:
     print(' '.join(w for w in phrase))
